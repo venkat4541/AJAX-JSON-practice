ae26346ea85acbe030f19c5ec5d704febafb474f
Venkat Polumahanti <venkat4541@gmail.com>
2017-02-19 01:43:42 -0500

    Checking connectivity to database through PHP

--- temp.php
+++ temp.php
@@ -8,4 +8,13 @@ echo "Last Name = ".$last;
 echo "<br>";
 echo "Age = ".$age;
 echo "<br>";
+
+$con = new mysqli("localhost","venkekno_admin","@TG7TEr%\$a)y");
+$sql = "SELECT * FROM 'names'";
+
+if($con->ping()) {
+  echo "connected";
+} else {
+  echo "not connected";
+}
 ?>
