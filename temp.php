77340f885710cd95124467fdee60a6f8ff5a7526
Venkat Polumahanti <venkat4541@gmail.com>
2017-02-19 16:15:41 -0500

    Once connected to DB, insert and return ID which is auto-incremented

--- temp.php
+++ temp.php
@@ -2,19 +2,19 @@
 $first=$_POST['fname'];
 $last=$_POST['lname'];
 $age=$_POST['age'];
-echo "First Name = ".$first;
-echo "<br>";
-echo "Last Name = ".$last;
-echo "<br>";
-echo "Age = ".$age;
-echo "<br>";
+echo "First Name = ".$first."<br>";
+echo "Last Name = ".$last."<br>";
+echo "Age = ".$age."<br>";
 
-$con = new mysqli("localhost","venkekno_admin","@TG7TEr%\$a)y");
-$sql = "SELECT * FROM 'names'";
+$con = new mysqli("localhost","venkekno_admin","@TG7TEr%\$a)y","venkekno_ajax_test");
+$sql = "INSERT INTO names (first, last, age) VALUES ('".$first."','".$last."','".$age."')";
 
-if($con->ping()) {
-  echo "connected";
+if($con->query($sql) === TRUE) {
+  echo "<br>";
+  echo "Created entry ".$con->insert_id;
 } else {
-  echo "not connected";
+  echo "Issue creating entry <br>".$con->error;
 }
+
+$con->close();
 ?>
